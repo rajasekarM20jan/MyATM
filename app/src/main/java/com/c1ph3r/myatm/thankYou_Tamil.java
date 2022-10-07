f97ef97c534213e6a2630f831e6c737cda89824c
Temp-user4 <rs20jan1998s@gmail.com>
2022-10-07 09:57:48 +0530

    init

---
+++ app/src/main/java/com/c1ph3r/myatm/thankYou_Tamil.java
@@ -0,0 +1,14 @@
+package com.c1ph3r.myatm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thankYou_Tamil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thank_you_tamil);
+    }
+}
